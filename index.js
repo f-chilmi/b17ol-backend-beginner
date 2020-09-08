ab9a5868d9044b44c326c7818dc2eed7a963dc4f
f-chilmi <fchilmi5@gmail.com>
2020-09-08 23:04:46 +0700

    GET (/home) method check

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/home', (req, res)=>{
+    res.send('Welcome to my first backend!')
+})
+
+app.listen(8080, ()=>{
+    console.log('App listening on port 8080')
+})
