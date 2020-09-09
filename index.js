687b00970b8a1368ff57e9a64d07c048b962e013
f-chilmi <fchilmi5@gmail.com>
2020-09-09 22:27:39 +0700

    pembahasan dan demo CRUD

--- index.js
+++ index.js
@@ -3,26 +3,62 @@ const bodyParser= require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(8080, ()=>{
+    console.log('App listening on port 8080')
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/home', (req, res)=>{
     res.send('Welcome to my first backend!')
 })
 
-app.use(express.static('assets'))
-
-app.get('/', (req, res)=>{
-    res.sendFile("index.html")
-})
-
 app.post('/data', (req, res)=>{
-    console.log('data:', req.body)
-    res.sendStatus(200)
+    console.log(req.body)
+    res.send('OK')
 })
 
 app.post('/profile', (req, res)=>{
-    
-    
+    res.send(req.body)
+})
+
+const data = {
+    name: 'Jane',
+    batch: '17.2',
+    email: 'janedoe@gmail.com'
+}
+
+app.patch('/profile', (req, res)=>{
+    const result = {
+        ...data,
+        ...req.body
+    }
+    res.send(result)
+})
+
+app.put('/profile', (req, res)=>{
+    const {name, batch, email} = req.body
+    if(name && batch && email){
+        var data = {
+            ...req.body
+        }
+        res.send(data)
+    }
+    else{
+        res.status(400).send({
+            success: false,
+            message: 'All form must be filled'
+        })
+    }
+})
+
+app.get('/profile', (req, res)=>{
+    res.send(data)
+})
+
+app.delete('/profile', (req, res)=>{
+    data = null,
+    res.send(data)
 })
 
 
@@ -32,6 +68,3 @@ app.post('/profile', (req, res)=>{
 
 
 
-app.listen(8080, ()=>{
-    console.log('App listening on port 8080')
-})
