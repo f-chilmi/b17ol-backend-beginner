f73cdf4c7e60867823c749f3a6f8fb005704b718
f-chilmi <fchilmi5@gmail.com>
2020-09-09 09:32:10 +0700

    POST (/data) check

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express')
+const bodyParser= require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/home', (req, res)=>{
     res.send('Welcome to my first backend!')
 })
+
+app.use(express.static('assets'))
+
+app.get('/', (req, res)=>{
+    res.sendFile("index.html")
+})
+
+app.post('/data', (req, res)=>{
+    console.log('data:', req.body)
+    res.sendStatus(200)
+})
+
+app.post('/profile', (req, res)=>{
+    
+    
+})
+
+
+
+
+
+
+
 
 app.listen(8080, ()=>{
     console.log('App listening on port 8080')
